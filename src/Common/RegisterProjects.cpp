73050e51b25fd597c44214ee09e7d4a1929a997a
AnkushRoy-code <ankush3411111@gmail.com>
2025-03-23 21:30:32 +0530

    Removed Project: Particle
    After careful considerations. I've dropped the idea of adapting my old
    projects to my Garage. It's not that fun or has the enjoyment I had the
    first time I made it.
    
    Another big reason is batch rendering. I am not able to do that and I
    won't be able to make my Particle without it, more appropriately I think
    I don't want to. Not that I can do it. I can indeed learn it over a week
    but It doesn't interest me for now. I will work on simple other
    simulations instead of old ones. I might do a flock thingy next... oh
    fuck that will also need batch rendering... kill me...

--- src/Common/RegisterProjects.cpp
+++ src/Common/RegisterProjects.cpp
@@ -1,12 +1,10 @@
 #include "Common/BaseProject.h"
 #include "Projects/SimpleExample/SimpleExample.h"
 #include "Projects/Triangle/Triangle.h"
-#include "Projects/Particle/src/Main.h"
 #include <memory>
 
 void Common::ProjectManager::registerAllProjects()
 {
     ProjectManager::registerProject(std::make_unique<SimpleExample>());
     ProjectManager::registerProject(std::make_unique<Triangle>());
-    ProjectManager::registerProject(std::make_unique<Particle>());
 }
