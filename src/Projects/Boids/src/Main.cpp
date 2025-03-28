4e98e6a0d1eeaaec8682772ac8519ec0b355506f
AnkushRoy-code <ankush3411111@gmail.com>
2025-03-28 11:32:35 +0530

    Windows fix

--- src/Projects/Boids/src/Main.cpp
+++ src/Projects/Boids/src/Main.cpp
@@ -131,22 +131,22 @@ bool Boids::Update()
 Matrix4x4 Matrix4x4_CreateOrthographicOffCenter(
     float left, float right, float bottom, float top, float zNearPlane, float zFarPlane)
 {
-    return (Matrix4x4) {2.0f / (right - left),
-                        0,
-                        0,
-                        0,
-                        0,
-                        2.0f / (top - bottom),
-                        0,
-                        0,
-                        0,
-                        0,
-                        1.0f / (zNearPlane - zFarPlane),
-                        0,
-                        (left + right) / (left - right),
-                        (top + bottom) / (bottom - top),
-                        zNearPlane / (zNearPlane - zFarPlane),
-                        1};
+    return {2.0f / (right - left),
+            0,
+            0,
+            0,
+            0,
+            2.0f / (top - bottom),
+            0,
+            0,
+            0,
+            0,
+            1.0f / (zNearPlane - zFarPlane),
+            0,
+            (left + right) / (left - right),
+            (top + bottom) / (bottom - top),
+            zNearPlane / (zNearPlane - zFarPlane),
+            1};
 }
 
 bool Boids::Draw()
@@ -171,10 +171,10 @@ bool Boids::Draw()
 
         for (Uint32 i = 0; i < SPRITE_COUNT; i += 1)
         {
-            Sint32 ravioli      = 0;
-            dataPtr[i].x        = (i * 200) % (int)w;
-            dataPtr[i].y        = (i * 100) % (int)h;
-            dataPtr[i].z        = 0;
+            Sint32 ravioli = 0;
+            dataPtr[i].x   = (i * 200) % (int)w;
+            dataPtr[i].y   = (i * 100) % (int)h;
+            dataPtr[i].z   = 0;
             // dataPtr[i].rotation = SDL_PI_F * 2 * std::sin(Utils::Time::getTicks() / 1000.0f);
             dataPtr[i].rotation = SDL_PI_F * 2;
             dataPtr[i].w        = 32;
