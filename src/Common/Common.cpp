95b45f2256067b497001649dc007f96adf94ee00
AnkushRoy-code <ankush3411111@gmail.com>
2025-04-01 13:51:45 +0530

    Removed dependency: Stb_image.h

--- src/Common/Common.cpp
+++ src/Common/Common.cpp
@@ -9,13 +9,6 @@
 #include <fstream>
 #include <vector>
 
-#define STB_IMAGE_IMPLEMENTATION
-#define STBI_MALLOC  SDL_malloc
-#define STBI_REALLOC SDL_realloc
-#define STBI_FREE    SDL_free
-#define STBI_ONLY_HDR
-#include "stb_image.h"
-
 namespace Common
 {
 SDL_GPUShader *LoadShader(SDL_GPUDevice *device,
