1f1d5d06b84bc8e7dd00349a359b5a7f8c9157c4
AnkushRoy-code <ankush3411111@gmail.com>
2025-04-08 11:26:34 +0530

    made a script to compile the shaders

---
+++ res/Shaders/CompileShader.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p Compiled/MSL Compiled/SPIRV Compiled/DXIL
+
+for file in Source/*.hlsl; do
+    filename=$(basename "$file" .hlsl)
+
+    echo "Compiling $filename..."
+
+    # The compiler I use is downloaded from somewhere like this: https://github.com/libsdl-org/SDL_shadercross/actions/runs/14041160733/artifacts/2809508886
+    /home/ankush/Downloads/SDL3_shadercross-linux-x64/SDL3_shadercross-3.0.0-linux-x64/bin/shadercross "$file" -o "Compiled/MSL/${filename}.msl"
+    /home/ankush/Downloads/SDL3_shadercross-linux-x64/SDL3_shadercross-3.0.0-linux-x64/bin/shadercross "$file" -o "Compiled/SPIRV/${filename}.spv"
+    /home/ankush/Downloads/SDL3_shadercross-linux-x64/SDL3_shadercross-3.0.0-linux-x64/bin/shadercross "$file" -o "Compiled/DXIL/${filename}.dxil"
+done
