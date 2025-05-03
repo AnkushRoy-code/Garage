638904d887de1163c8edaea00f74324f9f1bf970
AnkushRoy-code <ankush3411111@gmail.com>
2025-05-03 12:25:32 +0530

    Small fixes

--- cmake/Release.cmake
+++ cmake/Release.cmake
@@ -15,28 +15,36 @@ set(CPACK_PACKAGE_VENDOR "Ankush Roy")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "A Collection of Ankush's Creation")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README.md")
-
 set(CPACK_PACKAGE_CONTACT "Ankush Roy <ankush3411111@gmail.com>")
-set (CPACK_PACKAGE_EXECUTABLES "Garage" "Garage" )
+set(CPACK_PACKAGE_EXECUTABLES "Garage" "Garage")
 
 # Windows (NSIS) - Options
-set(CPACK_NSIS_MUI_ICON "${CMake_SOURCE_DIR}/docs/Logo/AnkushGarage.ico")
-set(CPACK_NSIS_MUI_UNIICON "${CMake_SOURCE_DIR}/docs/Logo/AnkushGarage.ico")
+set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/docs/Logo/AnkushGarage.ico")
+set(CPACK_NSIS_MUI_UNIICON "${CMAKE_SOURCE_DIR}/docs/Logo/AnkushGarage.ico")
 set(CPACK_NSIS_INSTALLED_ICON_NAME "bin/Garage.exe")
 set(CPACK_NSIS_PACKAGE_NAME "Ankush's Garage ${PROJECT_VERSION}")
-set(CPACK_PACKAGE_ICON "${CMake_SOURCE_DIR}/docs/Logo/AnkushGarage.bmp")
+
+if(WIN32)
+    set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/docs/Logo\\\\AnkushGarage.bmp")
+endif()
 set(CPACK_NSIS_MODIFY_PATH ON)
 set(CPACK_CREATE_DESKTOP_LINKS Garage)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "Garage ${PROJECT_VERSION}")
 
 # Linux (Debain) - Options
+
 # set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${CPACK_PACKAGE_CONTACT}") # This is the default
 # set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "x86-64") # defaults to i386
 
 # Linux (RPM) - Options
+
 # None needed for now
 
 # MacOS (Drag and Drop) - Options
+if(!WIN32)
+    set(CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/docs/Logo/AnkushGarage.bmp")
+endif()
+
 # None needed for now
 
 # Naming the Packed(installer) file
@@ -55,6 +63,6 @@ set(CPACK_PACKAGE_FILE_NAME
 )
 
 # set(CPACK_GENERATOR "NSIS;DRAGNDROP;DEB;RPM;ZIP;TGZ")
-set(CPACK_GENERATOR "DEB;RPM;ZIP;TGZ")
+set(CPACK_GENERATOR "ZIP;TGZ")
 
 include(CPack)
