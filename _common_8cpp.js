4f4f6909216718b3fbc3e2a9b551effcf15fc9aa
AnkushRoy-code <AnkushRoy-code@users.noreply.github.com>
2025-05-18 10:26:36 +0000

    deploy: 4e7b418b0d2db407b8da391e8d2dcf84f46f438d

--- _common_8cpp.js
+++ _common_8cpp.js
@@ -1,5 +1,6 @@
 var _common_8cpp =
 [
+    [ "Common::GetBasePath", "namespace_common.html#a6dfd0e1843d3a1b4aa310f0835aed431", null ],
     [ "Common::LoadImage", "namespace_common.html#a6bfa706f5ce182cb55b0c50edb7e6066", null ],
     [ "Common::LoadShader", "namespace_common.html#ac6711603d8b911cc9205b543a7fef805", null ]
 ];
--- namespace_common.js
+++ namespace_common.js
@@ -3,6 +3,7 @@ var namespace_common =
     [ "BaseProject", "class_common_1_1_base_project.html", "class_common_1_1_base_project" ],
     [ "ImGuiUI", "class_common_1_1_im_gui_u_i.html", "class_common_1_1_im_gui_u_i" ],
     [ "ProjectManager", "class_common_1_1_project_manager.html", "class_common_1_1_project_manager" ],
+    [ "GetBasePath", "namespace_common.html#a6dfd0e1843d3a1b4aa310f0835aed431", null ],
     [ "LoadImage", "namespace_common.html#a6bfa706f5ce182cb55b0c50edb7e6066", null ],
     [ "LoadShader", "namespace_common.html#ac6711603d8b911cc9205b543a7fef805", null ]
 ];
