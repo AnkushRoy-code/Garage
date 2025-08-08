fda1303acdbaa527221ca955d286458b342a40ec
AnkushRoy-code <ankush3411111@gmail.com>
2025-08-08 18:05:15 +0530

    feat: new way of handling versions!!

---
+++ cmake/Version.cmake
@@ -0,0 +1,21 @@
+# Set the version for Garage
+find_package(Git)
+
+if(Git_FOUND AND GIT_EXECUTABLE)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --tags
+        OUTPUT_VARIABLE PROJECT_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+endif()
+
+if(${PROJECT_VERSION} STREQUAL "")
+    set(${PROJECT_VERSION} v0.0.0-unknown)
+
+    message(
+        WARNING
+            "GARAGE: Failed to determine version from Git tags. Using default version \"${FOO_VERSION}\"."
+    )
+endif()
+
+message(STATUS "GARAGE: Version: ${PROJECT_VERSION}")
