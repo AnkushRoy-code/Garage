27e744cc9b561d65f1e906991be202caa6e47457
AnkushRoy-code <ankush3411111@gmail.com>
2025-08-08 18:05:15 +0530

    feat: new way of handling versions!!

--- cmake/Initialise.cmake
+++ cmake/Initialise.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 if(NOT CMAKE_BUILD_TYPE)
-    set(default_build_type "Debug")
+    set(default_build_type "Release")
     message(STATUS "Set the build type to `${default_build_type}` as none was specified.")
     set(CMAKE_BUILD_TYPE
         ${default_build_type}
@@ -13,11 +13,7 @@ if(NOT CMAKE_BUILD_TYPE)
         CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo"
     )
 endif()
-message(STATUS "Garage Build Type: ${CMAKE_BUILD_TYPE}")
 
-# Set the version for Garage
-set(Garage_Version_Major 0)
-set(Garage_Version_Minor 2)
-set(Garage_Version_Patch 8)
-set(PROJECT_VERSION "${Garage_Version_Major}.${Garage_Version_Minor}.${Garage_Version_Patch}")
-message(STATUS "${PROJECT_NAME} version: ${PROJECT_VERSION}")
+message(STATUS "GARAGE: Build Type: ${CMAKE_BUILD_TYPE}")
+
+include(cmake/Version.cmake)
